2162f718ce515bd028ab70810c7c530ca4696242
josephzzq <joseph.tsai@tgc-taiwan.com.tw>
2017-10-18 14:33:28 +0800

    DESC: setup accountKit envirnonment

--- AccountKit/ViewController.m
+++ AccountKit/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <AccountKit/AccountKit.h>
 
-@interface ViewController ()
+@interface ViewController () <AKFViewControllerDelegate>
+{
+    AKFAccountKit *_accountKit;
+    UIViewController<AKFViewController> *akfVC;
+}
 
 @end
 
@@ -17,13 +22,77 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // initialize Account Kit
+    if (_accountKit == nil) {
+        // may also specify AKFResponseTypeAccessToken
+        _accountKit = [[AKFAccountKit alloc] initWithResponseType:AKFResponseTypeAuthorizationCode];
+    }
+    
+    akfVC = [_accountKit viewControllerForPhoneLoginWithPhoneNumber:nil state:nil];
+    akfVC.uiManager = [[AKFSkinManager alloc]initWithSkinType:AKFSkinTypeTranslucent primaryColor:[UIColor purpleColor]];
+    
+    [self _prepareLoginViewController:akfVC];
+    [self.view addSubview:akfVC.view];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)_prepareLoginViewController:(UIViewController<AKFViewController> *)loginViewController
+{
+    loginViewController.delegate = self;
+    loginViewController.whitelistedCountryCodes = @[@"TW"];
+    // Optionally, you may use the Advanced UI Manager or set a theme to customize the UI.
+    loginViewController.advancedUIManager = nil;
+    loginViewController.theme = [self bicycleTheme];
 }
+
+- (AKFTheme *)bicycleTheme
+{
+    AKFTheme *theme = [AKFTheme outlineThemeWithPrimaryColor:[self _colorWithHex:0xffff5a5f]
+                                            primaryTextColor:[UIColor whiteColor]
+                                          secondaryTextColor:[UIColor whiteColor]
+                                              statusBarStyle:UIStatusBarStyleLightContent];
+//    theme.backgroundImage = [UIImage imageNamed:@"bicycle"];
+    theme.backgroundColor = [self _colorWithHex:0x66000000];
+    theme.inputBackgroundColor = [self _colorWithHex:0x00000000];
+    theme.inputBorderColor = [UIColor whiteColor];
+    return theme;
+}
+
+- (UIColor *)_colorWithHex:(NSUInteger)hex
+{
+    CGFloat alpha = ((CGFloat)((hex & 0xff000000) >> 24)) / 255.0;
+    CGFloat red = ((CGFloat)((hex & 0x00ff0000) >> 16)) / 255.0;
+    CGFloat green = ((CGFloat)((hex & 0x0000ff00) >> 8)) / 255.0;
+    CGFloat blue = ((CGFloat)((hex & 0x000000ff) >> 0)) / 255.0;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
+
+#pragma mark - AKFVC delegate
+- (void)viewController:(UIViewController<AKFViewController> *)viewController didCompleteLoginWithAccessToken:(id<AKFAccessToken>)accessToken state:(NSString *)state{
+    
+    [_accountKit requestAccount:^(id<AKFAccount> account, NSError *error) {
+        NSLog(@"userInfo id : %@ ,token id: %@ , phone : %@ , AKFAccount %@",account.accountID,accessToken.accountID ,  [account.phoneNumber stringRepresentation] , account);
+    }];
+    
+}
+
+- (void)viewController:(UIViewController<AKFViewController> *)viewController didCompleteLoginWithAuthorizationCode:(NSString *)code state:(NSString *)state{
+    
+    NSLog(@"authorization %@",code);
+    
+}
+
+- (void)viewController:(UIViewController<AKFViewController> *)viewController didFailWithError:(NSError *)error{
+    NSLog(@"error %@",error.userInfo);
+}
+
+- (void)viewControllerDidCancel:(UIViewController<AKFViewController> *)viewController{
+    
+}
+
+
 
 
 @end
