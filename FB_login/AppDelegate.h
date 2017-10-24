32fcfc60ed8eba98157d5bf4f34eb775a851ca2f
josephzzq <joseph.tsai@tgc-taiwan.com.tw>
2017-10-24 11:43:13 +0800

    DESC: add FB login class

---
+++ FB_login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FB_login
+//
+//  Created by Joseph.Tsai on 2017/10/24.
+//  Copyright © 2017年 Joseph.Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FB_login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FB_login
+//
+//  Created by Joseph.Tsai on 2017/10/24.
+//  Copyright © 2017年 Joseph.Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
