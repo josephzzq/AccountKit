128c5ecfbba4b4eaed10952c226b06dd5685f46d
josephzzq <joseph.tsai@tgc-taiwan.com.tw>
2017-11-02 14:25:05 +0800

    DESC: setup FB login

--- FB_login/AppDelegate.m
+++ FB_login/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface AppDelegate ()
 
@@ -14,11 +16,36 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // Get token cache
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
+    // Add any custom logic here.
     return YES;
 }
+
+#pragma mark -OpenURL
+// iOS9>
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)option {
+   
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:app openURL:url options:option];
+    // Add any custom logic here.
+    return handled;
+
+}
+// iOS9<
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
+    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                                  openURL:url
+                                                        sourceApplication:sourceApplication
+                                                               annotation:annotation
+                    ];
+    // Add any custom logic here.
+    return handled;
+} 
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
--- FB_login/ViewController.m
+++ FB_login/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet FBSDKLoginButton *fbLogin;
 
 @end
 
@@ -16,14 +19,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.fbLogin.layer setCornerRadius:5.0f];
+    self.fbLogin.readPermissions = @[@"public_profile", @"email", @"user_friends",@"user_likes",@"user_birthday",@"user_actions.video"];
+    self.fbLogin.delegate=self;
+    
+    //friends -> only get amount , taggable_friends > can get , but need apply by FB
+    //likes -> OK
+    //birthday -> OK
+    //user_actions.video -> fail , but video.watches ->OK
+    //mobile_phone -> fail
+    
+    
+    if ([FBSDKAccessToken currentAccessToken]) {
+        // User is logged in, do work such as go to next view controller.
+    }
+    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - FBSDKLoginButtonDelegate
+- (void)loginButton:(FBSDKLoginButton *)loginButton
+didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result
+              error:(NSError *)error{
+    
+    if (error) {
+        NSLog(@"error code %ld",error.code);
+    }else{
+        NSLog(@"grantedPermissions are %@",result.grantedPermissions);
+        NSLog(@"declinedPermissions are %@",result.declinedPermissions);
+        NSLog(@"user cancel %d",result.isCancelled);
+        NSLog(@"token is %@",result.token);
+    }
+    
+}
+- (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton{
+    NSLog(@"user fb logout");
+}
+
+
+
+
 
 
 @end
